901ff4a10d5af6262e6e59c60f342b8378f454d6
Yuuka <yuuka67@outlook.com>
2025-03-19 23:09:55 +0800

    feat: remove @tailwindcss/typography to resolve style conflict

--- app/[lang]/legal/layout.tsx
+++ app/[lang]/legal/layout.tsx
@@ -13,7 +13,7 @@ export default function BlogLayout({
   return (
     <div className={`h-full ${styles.blog_layout}`}>
       <Header lang={params.lang} />
-      <div className="custom-container min-h-screen pt-36 prose dark:prose-invert">{children}</div>
+      <div className="custom-container min-h-screen pt-36">{children}</div>
       <Footer />
     </div>
   );
