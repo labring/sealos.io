b62fe7163f6acfbd5b10a1f8cb3650f24bcf3fad
Yiming <wang1m@outlook.com>
2025-03-20 16:05:47 +0800

    feat: add i18n & toc in blog page

--- app/[lang]/legal/layout.tsx
+++ app/[lang]/legal/layout.tsx
@@ -2,7 +2,6 @@ import Footer from '@/components/footer';
 import Header from '@/components/header';
 import { ReactNode } from 'react';
 import styles from './index.module.css';
-import clsx from 'clsx';
 
 export default function BlogLayout({
   params,
@@ -14,7 +13,9 @@ export default function BlogLayout({
   return (
     <div className={`h-full ${styles.blog_layout}`}>
       <Header lang={params.lang} />
-      <div className={clsx("custom-container min-h-screen pt-36", styles.markdown)}>{children}</div>
+      <div className="custom-container md:px-[15%]min-h-screen dark:prose-invert prose px-8 pt-36">
+        {children}
+      </div>
       <Footer />
     </div>
   );
