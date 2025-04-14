4f69c6e9a4177182bfc07855d78c95316fa90e9f
ShortCipher5 <me@shortcipher.me>
2025-04-14 23:03:04 +0800

    feat: Add YouTube video support

---
+++ components/docs/Video.tsx
@@ -0,0 +1,11 @@
+export default function YouTube({ id }: { id: string }) {
+  return (
+    <div className="border-2 border-black">
+      <iframe
+        className="aspect-video w-full"
+        src={`https://www.youtube.com/embed/${id}`}
+        title="YouTube Video Player"
+      />
+    </div>
+  );
+}
