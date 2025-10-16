ccf578d105215980eaa227823a789da08eca67df
Nixieboluo <me@sagirii.me>
2025-10-16 10:44:33 +0800

    feat: demo section
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- app/dev/new-home/sections/DemoSection.tsx
+++ app/dev/new-home/sections/DemoSection.tsx
@@ -1,60 +1,29 @@
-import React from 'react';
+'use client';
+
+import { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'motion/react';
 import { FeatureStepper } from '../components/FeatureStepper';
+import DemoLightSvg from '../assets/demo-light.svg';
 
 export function DemoSection() {
-  const filterId = React.useId();
-  const gradientId = React.useId();
+  const demoRef = useRef<HTMLDivElement>(null);
+
+  // 使用 useScroll 跟踪元素的滚动进度
+  const { scrollYProgress } = useScroll({
+    target: demoRef,
+    // 当元素顶边进入viewport底部时开始(start start)
+    // 当元素底边到达viewport底部时结束(end end)
+    offset: ['start end', 'end end'],
+  });
+
+  // 将滚动进度映射到旋转角度：从24度到0度
+  const rotateAngle = useTransform(scrollYProgress, [0, 1], [24, 0]);
 
   return (
     <section className="relative mt-28">
       {/* Light */}
       <div className="absolute top-0 -z-10 -mt-36 w-full">
-        <svg
-          fill="none"
-          viewBox="0 0 1263 532"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g filter={`url(#${filterId})`}>
-            <path
-              d="M632.021 150L1112.43 381.267H150L632.021 150Z"
-              fill={`url(#${gradientId})`}
-              fillOpacity=".3"
-            />
-          </g>
-          <defs>
-            <filter
-              id={filterId}
-              x="0"
-              y="0"
-              width="1262.4"
-              height="531.27"
-              colorInterpolationFilters="sRGB"
-              filterUnits="userSpaceOnUse"
-            >
-              <feFlood floodOpacity="0" result="BackgroundImageFix" />
-              <feBlend
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                result="effect1_foregroundBlur_528_2962"
-                stdDeviation="75"
-              />
-            </filter>
-            <radialGradient
-              id={gradientId}
-              cx="0"
-              cy="0"
-              r="1"
-              gradientTransform="translate(631.21 265.63) scale(481.21 115.63)"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#fff" offset="0" />
-              <stop stopColor="#0C0C0C" offset="1" />
-            </radialGradient>
-          </defs>
-        </svg>
+        <img src={DemoLightSvg.src} alt="" className="w-full" />
       </div>
 
       {/* Stepper */}
@@ -62,9 +31,15 @@ export function DemoSection() {
 
       {/* Demo */}
       <div className="perspective-midrange perspective-origin-center">
-        <div className="bg-background mt-12 aspect-video origin-[top_center] transform-[rotate3d(1,0,0,8deg)] rounded-4xl border-4">
+        <motion.div
+          ref={demoRef}
+          className="bg-background mx-auto mt-12 aspect-video max-h-[calc(100vh-8rem)] origin-[top_center] rounded-4xl border-4"
+          style={{
+            rotateX: rotateAngle,
+          }}
+        >
           Video here
-        </div>
+        </motion.div>
       </div>
     </section>
   );
