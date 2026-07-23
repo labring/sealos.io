ce675b1481988aeec6c59fe07328a16552aa6dd7
yangchuansheng <yangchuansheng33@gmail.com>
2026-07-23 18:20:49 +0800

    fix(blog): render image blocks outside paragraphs

--- app/[lang]/(home)/blog/[slug]/page.tsx
+++ app/[lang]/(home)/blog/[slug]/page.tsx
@@ -49,10 +49,12 @@ export default async function BlogPage({
               children,
               ...props
             }: React.HTMLAttributes<HTMLParagraphElement>) => {
-              const hasH5 = React.Children.toArray(children).some(
-                (child) => React.isValidElement(child) && child.type === 'h5',
+              const hasBlockChild = React.Children.toArray(children).some(
+                (child) =>
+                  React.isValidElement<{ src?: unknown }>(child) &&
+                  (child.type === 'h5' || child.props.src !== undefined),
               );
-              if (hasH5) {
+              if (hasBlockChild) {
                 return <div {...props}>{children}</div>;
               }
               return <p {...props}>{children}</p>;
